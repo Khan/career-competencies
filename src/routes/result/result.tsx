dbd3efb2ab39e91900e5c9f9dc72cef287781ec0
Ned Redmond <edward@khanacademy.org>
2023-10-31 11:09:11 -0400

    basic table

--- src/routes/result/result.tsx
+++ src/routes/result/result.tsx
@@ -1,2 +1,27 @@
-// stub
-export const Result = () => <>🙂</>;
+import { Matrix } from "../../data";
+import "./result.css";
+
+export const Result = () => {
+  const competenciesForUser = Matrix.byCompetency;
+
+  return (
+    <table id="result-table">
+      <thead>
+        <tr>
+          <th scope="col">Competency</th>
+          <th scope="col">Expectation</th>
+        </tr>
+      </thead>
+      <tbody>
+        {competenciesForUser.map((competency) => (
+          <>
+            <tr>
+              <th scope="row">{competency.title}</th>
+              <td>Test</td>
+            </tr>
+          </>
+        ))}
+      </tbody>
+    </table>
+  );
+};
