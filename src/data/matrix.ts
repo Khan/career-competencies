163f1b89acaa519337e5b07b72477a895ebc6d98
Maddy Andrade <maddy531@gmail.com>
2023-11-01 13:20:34 -0400

    Convert matric into function.
    It now takes in tracks as an argument

--- src/data/matrix.ts
+++ src/data/matrix.ts
@@ -3,12 +3,13 @@ import { Competencies } from "./competencies";
 import { Skills } from "./skills";
 import type { Expectation } from "./expectations";
 import { Expectations } from "./expectations";
+import type { Track } from "./tracks";
 
 const PrimaryExpectations = Expectations.filter(
   (e) => !e.partial,
 ) as readonly Expectation[];
 
-export const Matrix: {
+type MatrixType = (track?: Track) => {
   byCompetency: (Competency & {
     expectations: (Expectation & {
       skills: (typeof Skills)[keyof typeof Skills][];
@@ -19,27 +20,35 @@ export const Matrix: {
       skills: (typeof Skills)[keyof typeof Skills][];
     })[];
   })[];
-} = {
-  byCompetency: Competencies.map((competency) => ({
-    ...competency,
-    expectations: PrimaryExpectations.map((expectation) => ({
-      ...expectation,
-      skills: Object.values(Skills).filter(
-        (skill) =>
-          skill.competency === competency.key &&
-          skill.expectation === expectation.key,
-      ),
-    })),
-  })),
-  byExpectation: PrimaryExpectations.map((expectation) => ({
-    ...expectation,
-    competencies: Competencies.map((competency) => ({
-      ...competency,
-      skills: Object.values(Skills).filter(
-        (skill) =>
-          skill.competency === competency.key &&
-          skill.expectation === expectation.key,
-      ),
-    })),
-  })),
 };
+
+
+const filterByTrack = (selectedTrack, competency: Competency) => {
+  return !selectedTrack || competency.track === "Core" || selectedTrack === competency.track;
+}
+
+export const Matrix: MatrixType = (selectedTrack) => (
+  {
+    byCompetency: Competencies.filter((competency) => filterByTrack(selectedTrack, competency)).map((competency) => ({
+      ...competency,
+      expectations: PrimaryExpectations.map((expectation) => ({
+        ...expectation,
+        skills: Object.values(Skills).filter(
+          (skill) =>
+            skill.competency === competency.key &&
+            skill.expectation === expectation.key,
+        ),
+      })),
+    })),
+    byExpectation: PrimaryExpectations.map((expectation) => ({
+      ...expectation,
+      competencies: Competencies.filter((competency) => filterByTrack(selectedTrack, competency)).map((competency) => ({
+        ...competency,
+        skills: Object.values(Skills).filter(
+          (skill) =>
+            skill.competency === competency.key &&
+            skill.expectation === expectation.key,
+        ),
+      })),
+    })),
+  });
