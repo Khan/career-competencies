0a0111396d1c4ade9b40860af485e308be206d21
Ned Redmond <edward@khanacademy.org>
2023-11-02 15:39:22 -0400

    add functional areas to determine competencies instead of tracks

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,11 +1,73 @@
-export const Tracks = [
-  "Data Insights",
-  "IT Professional",
+export interface Track {
+  readonly key: string;
+  readonly title: string;
+  readonly management: boolean;
+  readonly functions: readonly FunctionalArea[];
+}
+
+export const FunctionalAreas = [
+  "Engineering", // comprises Software Engineer, Quality Engineer, Data Insights, and others
   "Management",
-  "Software Engineer",
-  "Quality Analyst",
-  "Quality Engineer",
-  "Core",
+  "Core", // applies to all tracks in org
 ] as const;
 
-export type Track = (typeof Tracks)[number];
+export type FunctionalArea = (typeof FunctionalAreas)[number];
+
+export const Tracks: readonly Track[] = [
+  {
+    key: "core",
+    title: "Other",
+    management: false,
+    functions: ["Core"],
+  },
+  {
+    key: "dataInsights",
+    title: "Data Insights",
+    management: false,
+    functions: ["Engineering", "Core"],
+  },
+  {
+    key: "itProfessional",
+    title: "IT Professional",
+    management: false,
+    functions: ["Engineering", "Core"],
+  },
+  {
+    key: "engManagement",
+    title: "Management, Engineering",
+    management: true,
+    functions: ["Engineering", "Management", "Core"],
+  },
+  {
+    key: "softwareEngineer",
+    title: "Software Engineer",
+    management: false,
+    functions: ["Engineering", "Core"],
+  },
+  {
+    key: "qualityAnalyst",
+    title: "Quality Analyst",
+    management: false,
+    functions: ["Engineering", "Core"],
+  },
+  {
+    key: "qualityEngineer",
+    title: "Quality Engineer",
+    management: false,
+    functions: ["Engineering", "Core"],
+  },
+  {
+    key: "otherManagement",
+    title: "Management, Other",
+    management: true,
+    functions: ["Management", "Core"],
+  },
+] as const;
+
+export const getTrack = (track: string): Track => {
+  const found = Tracks.find((t) => t.key === track);
+  if (!found) {
+    throw new Error(`Track not found: ${track}`);
+  }
+  return found;
+};
