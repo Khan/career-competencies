93fd3b4d4522016911818659fab324c59df17765
Ned Redmond <edward@khanacademy.org>
2023-11-02 12:23:08 -0400

    fix test types

--- src/data/matrix.test.ts
+++ src/data/matrix.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from "@jest/globals";
 
 import { Matrix } from ".";
 import type { MatrixCompetencyItem, MatrixExpectationItem } from "./matrix";
