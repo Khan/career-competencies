19b8bfed138958888ebcee73f2501400721d0c68
Ned Redmond <nedredmond@pm.me>
2023-11-10 23:42:39 -0500

    prettier

--- src/routes/overview/overview.tsx
+++ src/routes/overview/overview.tsx
@@ -7,9 +7,7 @@ export const Overview = () => {
   const { user } = useData();
   return (
     <span id="overview">
-      <h3>
-        Competencies: {user?.track?.functions.join(", ") ?? "Core"}
-      </h3>
+      <h3>Competencies: {user?.track?.functions.join(", ") ?? "Core"}</h3>
       {Matrix(user?.track ?? getTrack("core")).byCompetency.map(
         (competency) => (
           <CompetencyItem key={competency.key} competency={competency} />
